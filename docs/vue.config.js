31990bbaee5eb767d75f67b13df61dc91ced9900
Mi1kDev <70041990+Mi1kDev@users.noreply.github.com>
2024-11-09 21:05:40 +0000

    Update vue.config.js

--- docs/vue.config.js
+++ docs/vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === "production" ? "/CST3144_CW/" : "/",
+    publicPath: process.env.NODE_ENV === "production" ? "/CST3144_CW/docs/" : "/",
   };
